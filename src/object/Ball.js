8335f2f9513d645a3ed5a9e9d32995866dd39268
trevsm <trevsm@pm.me>
2020-12-26 15:31:26 -0700

    Reset Movement of string with each frame

--- src/object/Ball.js
+++ src/object/Ball.js
@@ -1,19 +1,18 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useFrame } from 'react-three-fiber'
 import { useSphere } from '@react-three/cannon'
 
-const bpm = 186/2 // beats per min
+const bpm = 86 // beats per min
 const r = 3 // radius
 const count = 3 // number of balls
 const ballSize = 0.2 // size of each ball
 const p0 = [0, 1, 0] // ball position
 
 export const Ball = props => {
-
-  // const position = useRef([0, 0, 0])
+  const position = useRef([0, 0, 0])
 
   const [ref, api] = useSphere(() => ({
-    mass: 1,
+    mass: 0,
     position: [0, 0, 0],
     size: [props.ballSize, 32, 32],
   }))
@@ -23,17 +22,33 @@ export const Ball = props => {
   const t = useRef(props.startPos)
 
   useFrame(() => {
-    t.current += props.speed / 10000 + 0.0028
+    t.current += props.speed / 10000 + 0.0027
 
-    ref.current.position.x =
+    position.current[0] =
       pos[0] +
       (props.radius * Math.cos(t.current)) /
         (1 + Math.pow(Math.sin(t.current), 2))
 
-    ref.current.position.y =
+    position.current[1] =
       pos[1] +
       (props.radius * Math.sin(t.current) * Math.cos(t.current)) /
         (1 + Math.pow(Math.sin(t.current), 2))
+
+    const v = [
+      pos[0] +
+        (props.radius * Math.cos(t.current)) /
+          (1 + Math.pow(Math.sin(t.current), 2)),
+      pos[1] +
+        (props.radius * Math.sin(t.current) * Math.cos(t.current)) /
+          (1 + Math.pow(Math.sin(t.current), 2)),
+      0,
+    ]
+
+    api.position.set(v[0], v[1], v[2])
+  })
+
+  useEffect(() => {
+    api.position.subscribe(p => (position.current = p))
   })
 
   return (
--- src/object/String.js
+++ src/object/String.js
@@ -3,19 +3,32 @@ import { useBox } from '@react-three/cannon'
 import { useFrame } from 'react-three-fiber'
 
 export const String = props => {
-
   const [ref, api] = useBox(() => ({
     mass: 1,
     position: [0, 1, 0],
-    args:[2, 0.1, 0.1],
-    onCollide: () => {
-      console.log('collision!')
+    size: [2, 0.1, 0.1],
+    type: 'Dynamic',
+    onCollide: (e) => {
+      console.log(e.target)
     },
+    tolerance: 10,
   }))
 
-  useFrame(()=>{
-      ref.current.position.x = 0
-      ref.current.position.y = 2
+  function resetMovement() {
+    api.position.set(0, 1, 0)
+    api.rotation.set(0, 0, 0)
+
+    ref.current.rotation.x = 0
+    ref.current.rotation.y = 0
+    ref.current.rotation.z = 0
+
+    ref.current.position.x = 0
+    ref.current.position.y = 1
+    ref.current.position.z = 0
+  }
+
+  useFrame(() => {
+    resetMovement()
   })
 
   return (
